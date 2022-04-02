f1c275d80530824480b0ddea3238608b9ae1af04
jesusaviladev <avijesus13@gmail.com>
2022-04-02 14:47:23 -0400

    changed database integration

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -8,7 +8,7 @@ const config = {
 	dbUser: process.env.DB_USER,
 	dbPassword: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
-	DBEngine: 'mysql',
+	DBEngine: process.env.DB_ENGINE,
 };
 
 module.exports = config;
